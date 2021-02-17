f597b88ed710b46997368cac09e6ec7d2abb7137
Idury Jnana Bhaskar Akhil <bhaskarakhil29@gmail.com>
2021-02-17 18:44:15 +0530

    removeLoop question running successfully.

---
+++ LinkedLists/removeLoopGFG.cpp
@@ -0,0 +1,140 @@
+// { Driver Code Starts
+// driver code
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+bool isLoop(Node* head)
+{
+    if(!head) return false;
+    
+    Node* fast = head->next;
+    Node* slow = head;
+    
+    while( fast != slow)
+    {
+        if( !fast || !fast->next ) return false;
+        fast=fast->next->next;
+        slow=slow->next;
+    }
+    
+    return true;
+}
+
+int length(Node* head)
+{
+    int ret = 0;
+    while(head)
+    {
+        ret++;
+        head = head->next;
+    }
+    return ret;
+}
+
+void removeLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        removeLoop(head);
+        
+        if( isLoop(head) || length(head)!=n )
+            cout<<"0\n";
+        else
+            cout<<"1\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+structure of linked list node:
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+*/
+
+void removeLoop(Node* head)
+{
+    // code here
+    // just remove the loop without losing any nodes
+    Node* fst,*snd;
+    int flag=0;
+    for(fst=head,snd=head;fst&&snd&&snd->next;){
+        fst=fst->next;
+        snd=snd->next->next;
+        if(fst==snd){
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0){
+        return;
+    }
+    else{
+        fst=head;
+        while(fst!=snd){
+            fst=fst->next;
+            snd=snd->next;
+        }
+        while(fst->next!=snd){
+            fst=fst->next;
+        }
+        fst->next=NULL;
+    }
+}
